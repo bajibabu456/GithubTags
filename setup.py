5d8b126019b7933fa2892cb5499b76d0380cd8a2
BAJI SHAIK <bajishaik@MacBookAir.local>
2019-02-22 18:22:53 +0530

    add setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,9 @@
-import os
-import codecs
-
 from setuptools import setup
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-about = {}
-with codecs.open(os.path.join(here, 'nferx_py', '__version__.py'), 'r', 'utf-8') as f:
-    exec(f.read(), about)
-
-setup(name=about['__title__'],
-      version=about['__version__'],
-      description=about['__description__'],
-      author=about['__author__'],
-      packages=['GithubTags'],
-      install_requires=[
-          'requests'
-      ],
-      include_package_data=True)
+setup(
+    packages=['GithubTags'],
+    install_requires=[
+        'requests==2.9.1'
+    ],
+    include_package_data=True
+)
