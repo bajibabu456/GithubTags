61043bf0b5e225df00137cebd7dcfacfe6f728ba
BAJI SHAIK <bajishaik@MacBookAir.local>
2019-02-22 18:46:42 +0530

    add setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import setup
 
 setup(
     packages=['GithubTags'],
-    url='https://github.com/bajibabu9/GithubTags',
-    download_url='https://github.com/bajibabu9/GithubTags',
     install_requires=[
         'requests==2.9.1'
     ],
